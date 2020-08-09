99dacba8109438ff973ae38051a50ef797c13ab0
blogg9ggg <2361915181@qq.com>
2020-08-09 19:27:39 +0800

    DP

---
+++ P67.cpp
@@ -0,0 +1,22 @@
+/*
+*   DP
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[200][200];
+int inp[200][200];
+int main(){
+    freopen("p067_triangle.txt","r",stdin);
+    int n=100;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++)   scanf("%d",&inp[i][j]);
+    }
+    for(int j=1;j<=n;j++)  dp[n][j]=inp[n][j];
+    for(int i=n-1;i>0;i--){
+        for(int j=1;j<=i;j++){
+            dp[i][j]=inp[i][j]+max(dp[i+1][j],dp[i+1][j+1]);
+        }
+    }
+    printf("%d\n",dp[1][1]);
+}
